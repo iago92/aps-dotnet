0a2d921bb7a334367215d1c2a182abf22169510d
Iago <130412153+iago92@users.noreply.github.com>
2026-04-29 00:10:11 +0000

    aa

--- University.Infrastructure/Repositories/StudentRepository.cs
+++
@@ -1,7 +0,0 @@
-namespace University.Infrastructure.Repositories
-{
-    // Classe em desuso (mantida apenas para compatibilidade). Use AlunoRepositorio em português.
-    public class StudentRepository
-    {
-    }
-}
